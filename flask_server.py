2b8077d14bfca625c15ae124123440f02a73baef
Root <root@root.com>
2025-11-09 16:54:54 +0000

    Coffee was good

--- flask_server.py
+++ flask_server.py
@@ -363,4 +363,4 @@ if __name__ == "__main__":
     print("=" * 70)
 
     # Run the Flask app
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=6002, debug=False)
